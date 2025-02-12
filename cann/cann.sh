6a44d6e10a4187ed69a3b3176ba60fbdcc5052bb
Yuanhao Ji <jiyuanhao@apache.org>
2025-02-12 16:38:00 +0800

    revert

--- cann/cann.sh
+++ cann/cann.sh
@@ -2,85 +2,83 @@
 
 set -e
 
-# bash fonts colors
-cyan='\e[96m'
-yellow='\e[33m'
-red='\e[31m'
-none='\e[0m'
-
-_cyan() { echo -e "${cyan}$*${none}"; }
-_yellow() { echo -e "${yellow}$*${none}"; }
-_red() { echo -e "${red}$*${none}"; }
-
-_info() { _cyan "Info: $*"; }
-_warn() { _yellow "Warn: $*"; }
-_err() { _red "Error: $*" && exit 1; }
-
-_get_architecture() {
+get_architecture() {
     # not case sensitive
     shopt -s nocasematch
 
     case "${PLATFORM}" in
         "linux/x86_64"|"linux/amd64")
-            arch="x86_64"
+            ARCH="x86_64"
             ;;
         "linux/aarch64"|"linux/arm64")
-            arch="aarch64"
+            ARCH="aarch64"
             ;;
         *)
-            _err "Unsupported architecture ${PLATFORM}."
+            echo "Error: Unsupported architecture ${PLATFORM}."
+            exit 1
             ;;
     esac
 
-    echo "${arch}"
+    echo "${ARCH}"
 }
 
-_retry() {
-    "$@" || (sleep 10 && "$@") || (sleep 20 && "$@") || (sleep 40 && "$@")
-}
+download_file() {
+    set +e
 
-_download_file() {
-    _info "Downloading file from $1"
-    _retry curl -fsSL -o "$2" "$1"
+    local max_retries=10
+    local retry_delay=10
+    local url="$1"
+    local path="$2"
+    echo "Downloading ${path} from ${url}"
+
+    for ((i=1; i<=max_retries; i++)); do
+        echo "Attempt $i of $max_retries..."
+        curl -fsSL -o "${path}" "${url}"
+        if [[ $? -eq 0 ]]; then
+            return 0
+        else
+            echo "Download failed with error code $?. Retrying in ${retry_delay} seconds..."
+            sleep ${retry_delay}
+        fi
+    done
+
+    echo "All attempts failed. Exiting."
+    exit 1
 }
 
 download_cann() {
-    declare -A version_dict
-    version_dict["8.0.RC2.alpha001"]="V100R001C18B800TP015"
-    version_dict["8.0.RC2.alpha002"]="V100R001C18SPC805"
-    version_dict["8.0.RC2.alpha003"]="V100R001C18SPC703"
-    version_dict["8.0.RC3.alpha002"]="V100R001C19SPC702"
-
     local url="https://ascend-repo.obs.cn-east-2.myhuaweicloud.com"
-    if [[ ${CANN_VERSION} == *alpha* ]]; then
-        local version=${version_dict[${CANN_VERSION}]}
-        if [[ ${version} ]]; then
-            _err "version is not defined for CANN ${CANN_VERSION}"
-        fi
-        local url_prefix="${url}/Milan-ASL/Milan-ASL%20${version}"
+    if [[ ${CANN_VERSION} == "8.0.RC2.alpha001" ]]; then
+        local url_prefix="${url}/Milan-ASL/Milan-ASL%20V100R001C18B800TP015"
+    elif [[ ${CANN_VERSION} == "8.0.RC2.alpha002" ]]; then
+        local url_prefix="${url}/Milan-ASL/Milan-ASL%20V100R001C18SPC805"
+    elif [[ ${CANN_VERSION} == "8.0.RC2.alpha003" ]]; then
+        local url_prefix="${url}/Milan-ASL/Milan-ASL%20V100R001C18SPC703"
+    elif [[ ${CANN_VERSION} == "8.0.RC3.alpha002" ]]; then
+        local url_prefix="${url}/Milan-ASL/Milan-ASL%20V100R001C19SPC702"
     else
         local url_prefix="${url}/CANN/CANN%20${CANN_VERSION}"
     fi
+    local url_suffix="response-content-type=application/octet-stream"
+    local toolkit_url="${url_prefix}/${TOOLKIT_FILE}?${url_suffix}"
+    local kernels_url="${url_prefix}/${KERNELS_FILE}?${url_suffix}"
 
-    # Download cann-toolkit
     if [ ! -f "${TOOLKIT_PATH}" ]; then
-        local toolkit_url="${url_prefix}/${TOOLKIT_FILE}"
-        _download_file "${toolkit_url}" "${TOOLKIT_PATH}"
+        download_file "${toolkit_url}" "${TOOLKIT_PATH}"
     fi
 
-    # Download cann-kernels
     if [ ! -f "${KERNELS_PATH}" ]; then
-        local kernels_url="${url_prefix}/${KERNELS_FILE}"
-        _download_file "${kernels_url}" "${KERNELS_PATH}"
+        download_file "${kernels_url}" "${KERNELS_PATH}"
     fi
 
-    _info "CANN ${CANN_VERSION} download successful."
+    echo "CANN ${CANN_VERSION} download successful."
 }
 
 set_env() {
     local cann_toolkit_env_file="${CANN_HOME}/ascend-toolkit/set_env.sh"
     if [ ! -f "${cann_toolkit_env_file}" ]; then
-        _err "No such file: ${cann_toolkit_env_file}"
+        echo "CANN Toolkit ${CANN_VERSION} installation failed."
+        exit 1
     else
         local driver_path_env="LD_LIBRARY_PATH=${CANN_HOME}/driver/lib64/common/:${CANN_HOME}/driver/lib64/driver/:\${LD_LIBRARY_PATH}" && \
         echo "export ${driver_path_env}" >> /etc/profile
@@ -94,7 +92,7 @@ set_env() {
 install_cann() {
     # Download installers
     if [ ! -f "${TOOLKIT_PATH}" ] || [ ! -f "${KERNELS_PATH}" ]; then
-        _warn "Installers do not exist in the /tmp directory, re-download them."
+        echo "[WARNING] Installers do not exist in the /tmp directory, re-download them."
         download_cann
     fi
 
@@ -105,7 +103,7 @@ install_cann() {
         psutil protobuf==3.20 scipy requests absl-py
 
     # Install CANN Toolkit
-    _info "Installing ${TOOLKIT_FILE}"
+    echo "Installing ${TOOLKIT_FILE}"
     chmod +x "${TOOLKIT_PATH}"
     bash "${TOOLKIT_PATH}" --quiet --install --install-for-all --install-path="${CANN_HOME}"
     rm -f "${TOOLKIT_PATH}"
@@ -114,22 +112,22 @@ install_cann() {
     set_env
 
     # Install CANN Kernels
-    _info "Installing ${KERNELS_FILE}"
+    echo "Installing ${KERNELS_FILE}"
     chmod +x "${KERNELS_PATH}"
     bash "${KERNELS_PATH}" --quiet --install --install-for-all --install-path="${CANN_HOME}"
     rm -f "${KERNELS_PATH}"
 
-    _info "CANN ${CANN_VERSION} installation successful."
+    echo "CANN ${CANN_VERSION} installation successful."
 }
 
 PLATFORM=${PLATFORM:=$(uname -s)/$(uname -m)}
-ARCH=$(_get_architecture)
+ARCH=$(get_architecture)
 CANN_HOME=${CANN_HOME:="/usr/local/Ascend"}
 CANN_CHIP=${CANN_CHIP:="910b"}
-CANN_VERSION=${CANN_VERSION:="8.0.0"}
+CANN_VERSION=${CANN_VERSION:="8.0.RC1"}
 
 # NOTE: kernels are arch-specific after 8.0.RC3.alpha002
-if [[ ${CANN_VERSION} == "8.0.RC3" || ${CANN_VERSION} == "8.0.0" ]]; then
+if [[ ${CANN_VERSION} == "8.0.RC3" ]]; then
   KERNELS_ARCH="linux-${ARCH}"
 else
   KERNELS_ARCH="linux"
@@ -148,5 +146,6 @@ elif [ "$1" == "--install" ]; then
 elif [ "$1" == "--set_env" ]; then
     set_env
 else
-    _err "Unexpected arguments, use '--download', '--install' or '--set_env' instead"
+    echo "Unexpected arguments, use '--download', '--install' or '--set_env' instead"
+    exit 1
 fi
